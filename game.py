312e30e7d70bea4d6a290dc7e97ffca0d4223d0a
kelly-may <kellymay99@gmail.com>
2021-04-14 13:51:50 -0400

    updated deck_of_cards and started Player class

--- game.py
+++ game.py
@@ -3,21 +3,55 @@
 # written by Kelly May
 # 04/14/2021
 
-deck_of_cards = {"Ace of Clubs": 11, "Two of Clubs": 2, "Three of Clubs": 3, "Four of Clubs": 4, "Five of Clubs": 5,
-                 "Six of Clubs": 6, "Seven of Clubs": 7, "Eight of Clubs": 8, "Nine of Clubs": 9, "Ten of Clubs": 10,
-                 "Jack of Clubs": 10, "Queen of Clubs": 10, "King of Clubs": 10,
-                 "Ace of Diamonds": 11, "Two of Diamonds": 2, "Three of Diamonds": 3, "Four of Diamonds": 4,
-                 "Five of Diamonds": 5, "Six of Diamonds": 6, "Seven of Diamonds": 7, "Eight of Diamonds": 8,
-                 "Nine of Diamonds": 9, "Ten of Diamonds": 10,"Jack of Diamonds": 10, "Queen of Diamonds": 10,
-                 "King of Diamonds": 10,
-                 "Ace of Hearts": 11, "Two of Hearts": 2, "Three of Hearts": 3, "Four of Hearts": 4,
-                 "Five of Hearts": 5, "Six of Hearts": 6, "Seven of Hearts": 7, "Eight of Hearts": 8,
-                 "Nine of Hearts": 9, "Ten of Hearts": 10, "Jack of Hearts": 10, "Queen of Hearts": 10,
-                 "King of Hearts": 10,
-                 "Ace of Spades": 11, "Two of Spades": 2, "Three of Spades": 3, "Four of Spades": 4,
-                 "Five of Spades": 5, "Six of Spades": 6, "Seven of Spades": 7, "Eight of Spades": 8,
-                 "Nine of Spades": 9, "Ten of Spades": 10, "Jack of Spades": 10, "Queen of Spades": 10,
-                 "King of Spades": 10
+import random  # used to pick a random key from the dictionary
+
+# deck_of_cards: { card name : [isAce? , value, isAvailable?] }
+
+deck_of_cards = {"Ace of Clubs": [True, 11, True], "Two of Clubs": [False, 2, True], "Three of Clubs": [False, 3,True],
+                 "Four of Clubs": [False, 4, True], "Five of Clubs": [False, 5, True],
+                 "Six of Clubs": [False, 6, True], "Seven of Clubs": [False, 7, True] ,
+                 "Eight of Clubs": [False, 8, True], "Nine of Clubs": [False, 9, True],
+                 "Ten of Clubs": [False, 10, True], "Jack of Clubs": [False, 10, True],
+                 "Queen of Clubs": [False, 10, True], "King of Clubs": [False, 10, True],
+
+                 "Ace of Diamonds": [True, 11, True], "Two of Diamonds": [False, 2, True],
+                 "Three of Diamonds": [False, 3, True], "Four of Diamonds": [False, 4, True],
+                 "Five of Diamonds": [False, 5, True], "Six of Diamonds": [False, 6, True],
+                 "Seven of Diamonds": [False, 7, True], "Eight of Diamonds": [False, 8, True],
+                 "Nine of Diamonds": [False, 9, True], "Ten of Diamonds": [False, 10, True],
+                 "Jack of Diamonds": [False, 10, True], "Queen of Diamonds": [False, 10, True],
+                 "King of Diamonds": [False, 10, True],
+
+                 "Ace of Hearts": [True, 11, True], "Two of Hearts": [False, 2, True],
+                 "Three of Hearts": [False, 3, True], "Four of Hearts": [False, 4, True],
+                 "Five of Hearts": [False, 5, True], "Six of Hearts": [False, 6, True],
+                 "Seven of Hearts": [False, 7, True], "Eight of Hearts": [False, 8, True],
+                 "Nine of Hearts": [False, 9, True], "Ten of Hearts": [False, 10, True],
+                 "Jack of Hearts": [False, 10, True], "Queen of Hearts": [False, 10, True],
+                 "King of Hearts": [False, 10, True],
+
+                 "Ace of Spades": [True, 11, True], "Two of Spades": [False, 2,True],
+                 "Three of Spades": [False, 3, True], "Four of Spades": [False, 4, True],
+                 "Five of Spades": [False, 5, True], "Six of Spades": [False, 6, True],
+                 "Seven of Spades": [False, 7, True], "Eight of Spades": [False, 8, True],
+                 "Nine of Spades": [False, 9, True],  "Ten of Spades": [False, 10, True],
+                 "Jack of Spades": [False, 10, True],  "Queen of Spades": [False, 10, True],
+                 "King of Spades": [False, 10, True]
                  }
+
+
+class Player:
+    # class for defining each player (2 - 7 per game)
+    # maxes number of players
+    # assigns list of cards per player
+    # calculates total value for each player
+    card_list = []
+    card_total = 0
+
+    def __init__(self):
+        pass
+
+
+
 
 
